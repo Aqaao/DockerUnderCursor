3c03ce580d90704b6b0bed82463593e45fbfe556
Aqaao <Aqaao@outlook.com>
2024-05-05 21:17:43 +0800

    perf: eventfilter

--- dockerundercursor/dockermonitor.py
+++ dockerundercursor/dockermonitor.py
@@ -1,8 +1,8 @@
 from PyQt5.QtCore import QObject, QEvent
 # from PyQt5.QtGui import QCursor
 # from PyQt5.QtWidgets import QApplication
-# from PyQt5 import QtCore
-# from . import qt_event
+from PyQt5 import QtCore
+from . import qt_event
 class  DockerMonitor(QObject):
 
     def __init__(self,obj):
@@ -12,18 +12,11 @@ class  DockerMonitor(QObject):
         self.auto_conceal = False
 
     def eventFilter(self, obj, event):
-        if event.type() == QEvent.MouseButtonPress:
-            self.mouse_pressed = True
-        if event.type() == QEvent.MouseButtonRelease:
-            self.mouse_pressed = False
-            # if self.docker_manager.pinned:
-            #     pos =self.widget.pos()
-            #     if self.pin_position == pos:
-            #         self.pin_position = pos
-        # ==========DEBUG==========
-        # if self.docker_manager.name == "KisLayerBox" and self.docker_manager.widget == obj:
-        #     QtCore.qDebug(qt_event.event_lookup.get(str(event.type())))
         if self.auto_conceal:
+            if event.type() == QEvent.MouseButtonPress:
+                self.mouse_pressed = True
+            if event.type() == QEvent.MouseButtonRelease:
+                self.mouse_pressed = False
             if self.docker_manager.widget == obj and obj.isFloating() and not self.mouse_pressed:
                 #Leaves docker event.
                 if event.type() == QEvent.Leave:
